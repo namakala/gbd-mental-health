1d8a4503d59afe74378c5f66e155a4862b8d6c4c
lamurian <lamuri.aly8@gmail.com>
2023-08-23 00:42:16 +0200

    Run pipeline to generate report

--- _targets.R
+++ _targets.R
@@ -1,5 +1,5 @@
 # Load targets to build pipeline
-pkgs <- c("magrittr", "targets")
+pkgs <- c("magrittr", "targets", "renv", "docstring", "tarchetypes")
 sapply(pkgs, library, character.only = TRUE)
 
 # Set up package dependencies
@@ -12,11 +12,24 @@ funs <- list.files("src", pattern = "*R", recursive = TRUE, full.names = TRUE)
 sapply(funs, source)
 
 # List all data
-tbls <- list.files("data", recursive = TRUE, full.names = TRUE) %>%
+gbd_data <- list.files("data", recursive = TRUE, full.names = TRUE, pattern = "csv") %>%
   set_names(gsub(x = ., ".*/(.*).csv", "\\1"))
 
 # Analysis pipeline
 list(
-  tar_target(gbd_file, tbls["gbd-mental-illness"], format = "file"),
-  tar_target(tbl_gbd, readGBD(gbd_file, entity = c("Indonesia", "Income")))
+  tar_target(files, gbd_data),
+  tar_target(tbls, lapply(files, readGBD, entity = c("Income"))),
+  tar_target(
+    merge_tbl,
+    merge(
+      tbls[["psych-daly"]],
+      tbls[["psych-prev"]],
+      by = c("group", "ctry_code", "year"))
+  ),
+  tar_target(gbd_summary, groupSummary(merge_tbl, "group")),
+  tar_target(gbd_report, tblSummary(merge_tbl)),
+  tar_target(gbd_cor, groupCor(merge_tbl, "group") %>% lapply(corSummary)),
+  tar_target(viz_daly, swarmVizGBD(tbls[["psych-daly"]])),
+  tar_target(viz_prev, lineVizGBD(tbls[["psych-prev"]])),
+  tar_quarto(doc_descriptive, "docs/descriptive-stat.qmd")
 )
